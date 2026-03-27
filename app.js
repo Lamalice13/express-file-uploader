d7482dcdb9eacf9a1bd62d6de2f003bab63ad9b6
Lamalice13 <139547486+Lamalice13@users.noreply.github.com>
2026-03-27 16:00:52 +0100

    feat: set up express server

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+
+// CONFIG
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+// PORT
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, (e) => {
+  if (e) return console.log(e);
+  console.log(`Server running on ${PORT}`);
+});
